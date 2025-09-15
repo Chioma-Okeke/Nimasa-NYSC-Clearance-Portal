28c4cc5e3ed98c90b5c188b17cbfc6ea0b397ad2
Chioma-Okeke <okekegeraldenne@gmail.com>
2025-09-14 20:23:54 -0700

    feat: modified the loading spiinner size and color

--- components/shared/loading-spinner.tsx
+++ components/shared/loading-spinner.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const LoadingSpinner: React.FC<{ size?: string }> = ({ size = "h-8 w-8" }) => {
+const LoadingSpinner: React.FC<{ size?: string }> = ({ size = "h-5 w-5" }) => {
   return (
     <div className="flex items-center justify-center">
       <div
-        className={`animate-spin rounded-full border-4 border-gray-300 border-t-blue-500 ${size}`}
+        className={`animate-spin rounded-full border-4 border-gray-300 border-t-[#3eb56d] ${size}`}
       />
     </div>
   );
